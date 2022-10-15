c374ccf61593934f6294beaf6f015ea57f066988
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2022-10-15 17:30:04 -0400

    Create react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 import { GlobalStyle } from "./styles/global";
 
@@ -7,6 +9,10 @@ import { defaultTheme } from "./styles/themes/default";
 export function App() {
     return (
         <ThemeProvider theme={defaultTheme}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+
             <GlobalStyle />
         </ThemeProvider>
     );
