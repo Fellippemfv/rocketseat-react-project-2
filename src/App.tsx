0c68877cf2dec41a4053bf97071fcc3b042714d6
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2022-10-15 16:44:14 -0400

    Configure eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { ThemeProvider } from "styled-components"
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 
 import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default"
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-   <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="sucess" />
       <Button variant="danger" />
       <Button />
 
-
       <GlobalStyle />
-   </ThemeProvider>
-  )
+    </ThemeProvider>
+  );
 }
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -3,26 +3,26 @@ import styled, { css } from "styled-components";
 export type ButtonVariant = "primary" | "secondary" | "danger" | "sucess";
 
 interface ButtonContainerProps {
-    variant: ButtonVariant;
+  variant: ButtonVariant;
 }
 
 const buttonVariants = {
-    primary: "purple",
-    secondary: "orange",
-    danger: "red",
-    sucess: "green"
-}
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  sucess: "green",
+};
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
+  width: 100px;
+  height: 40px;
 
-    background-color: ${props => props.theme["green-500"]};
-    color: ${props => props.theme.white}
-/* 
-    ${props => {
-        return css `
-            background-color: ${buttonVariants[props.variant]}
-        `
-    }} */
-`
+  background-color: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme.white};
+  /* 
+    ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `;
+  }} */
+`;
