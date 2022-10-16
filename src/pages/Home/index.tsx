eb561f2576325ab97dddf8e7c2f48d11db315de5
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2022-10-16 14:18:59 -0400

    Update input home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,14 +17,25 @@ export function Home() {
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
                         id="task"
+                        list="task-suggestions"
                         placeholder="Dê um nome para o seu projeto"
                     />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto1" />
+                        <option value="Projeto2" />
+                        <option value="Projeto3" />
+                        <option value="Projeto4" />
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesAmountInput
                         type="number"
                         id="minutesAmount"
                         placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
                     />
 
                     <span>minutos</span>
